2e4a43c7191d5398bb3717e8eb6c140d4f4f0e85
keyboardmedic <keyboardmedic@proton.me>
2025-08-07 20:13:27 +0200

    add: added missing error handling to get_streamers_from_file remove: removed unneeded logic

--- src/going_live.py
+++ src/going_live.py
@@ -32,12 +32,14 @@ read_twitch_api_token_from_file = twitch_api_handler.read_twitch_api_token_from_
 get_token_from_twitch_api = twitch_api_handler.get_token_from_twitch_api
 get_stream_json_from_twitch = twitch_api_handler.get_stream_json_from_twitch
 
+time_before_retry = 60
+
 # functions
 def log_exception(type, value, tb):
     logger.exception("Uncaught exception: {0}".format(str(value)))
 
 # gets list of streamers to poll
-def get_streamers_from_file() -> tuple[list, bool]:
+def get_streamers_from_file() -> list:
     with open("config/streamers.txt", 'r') as file_with_streamer_ids:
         list_of_streamers = [line.rstrip() for line in file_with_streamer_ids]
         if "http" in list_of_streamers[0]:
@@ -45,19 +47,23 @@ def get_streamers_from_file() -> tuple[list, bool]:
             while error_count < 4:
                 try:
                     get_streamers_trough_request_response = requests.get(list_of_streamers[0])
-                    list_of_streamers = get_streamers_trough_request_response.text.splitlines()
-                    is_get_streamers_from_file_succesfull = get_streamers_trough_request_response.ok
+                    if get_streamers_trough_request_response.ok:
+                        list_of_streamers = get_streamers_trough_request_response.text.splitlines()
+                        break
+                    else:
+                        logger.error('was unable to get list of streamers trough request with response: %s with exception: %s waiting for %s seconds', get_streamers_trough_request_response, time_before_retry)
+                        error_count = error_count+1
+                        time.sleep(time_before_retry)
                 except Exception as e:
+                    logger.error('was unable to get list of streamers trough request with response: %s with exception: %s waiting for %s seconds', get_streamers_trough_request_response, e, time_before_retry)
                     error_count = error_count+1
-                    logger.error('was unable to get list of streamers trough request with response: %s with exception: %s', get_streamers_trough_request_response, e)
+                    time.sleep(time_before_retry)
             if error_count == 4:
                 raise RuntimeError("tried to get list of streamers trough url 3 times and failed")
-        else:
-            is_get_streamers_from_file_succesfull = True
 
     logger.info('list of streamers to poll from: %s', list_of_streamers)
 
-    return(list_of_streamers, is_get_streamers_from_file_succesfull)
+    return(list_of_streamers)
 
 # creates embeds folder if not exist allready
 def create_embeds_folder():
@@ -96,51 +102,50 @@ def main():
     
     logger.info("pulling list of streamers once to clean up old messages")
     
-    streamers, streamer_get_was_succesfull = get_streamers_from_file()
+    streamers = get_streamers_from_file()
     clean_up_old_embeds(streamers, loaded_config.use_offline_messages)
 
     # main loop
     while True:
-        streamers, streamer_get_was_succesfull = get_streamers_from_file()
-        if streamer_get_was_succesfull:
-            for streamer in streamers:
-                # sets streamer name incase an exception is called before streamer_name is set with a function
-                streamer_name = "unknown"
-                # gets streamer data from twitch api
+        streamers = get_streamers_from_file()
+        for streamer in streamers:
+            # sets streamer name incase an exception is called before streamer_name is set with a function
+            streamer_name = "unknown"
+            # gets streamer data from twitch api
+            get_stream_json_from_twitch_response,get_stream_json_from_twitch_data,is_live,stream_category,streamer_name = get_stream_json_from_twitch(streamer,token_from_twitch)
+            # if request to twitch api fails requests a new token from twitch and tries again
+            if not get_stream_json_from_twitch_response.ok:
+                token_from_twitch = get_token_from_twitch_api()
                 get_stream_json_from_twitch_response,get_stream_json_from_twitch_data,is_live,stream_category,streamer_name = get_stream_json_from_twitch(streamer,token_from_twitch)
-                # if request to twitch api fails requests a new token from twitch and tries again
-                if not get_stream_json_from_twitch_response.ok:
-                    token_from_twitch = get_token_from_twitch_api()
-                    get_stream_json_from_twitch_response,get_stream_json_from_twitch_data,is_live,stream_category,streamer_name = get_stream_json_from_twitch(streamer,token_from_twitch)
-                # checks if streamer is in allowed categories or if allowed categories is empty
-                if is_live and (stream_category.lower() in loaded_config.allowed_categories or len(loaded_config.allowed_categories)==0):
-                    
-                    if stream_category.lower() in loaded_config.allowed_categories:
-                        logger.info('%s for %s with name %s is found in allowed categories: %s', stream_category, streamer, streamer_name, loaded_config.allowed_categories)
-                    # updates embed if it allready exsists or creates it if not to discord webhook
-                    if exists(f"config/embeds/{streamer}.txt"):
-                        logger.info('embed allready exsists for %s with name %s, updating it',streamer, streamer_name)
-                        message_id_from_file,name_from_file = read_message_id_from_file(streamer)
-                        discord_webhook_edit(get_stream_json_from_twitch_data,message_id_from_file)
-                    else:
-                        logger.info('no embed exsists for %s with name %s, creating it',streamer, streamer_name)
-                        message_id = discord_webhook_send(get_stream_json_from_twitch_data)
-                        save_message_id_to_file(streamer,message_id,streamer_name)
+            # checks if streamer is in allowed categories or if allowed categories is empty
+            if is_live and (stream_category.lower() in loaded_config.allowed_categories or len(loaded_config.allowed_categories)==0):
                 
+                if stream_category.lower() in loaded_config.allowed_categories:
+                    logger.info('%s for %s with name %s is found in allowed categories: %s', stream_category, streamer, streamer_name, loaded_config.allowed_categories)
+                # updates embed if it allready exsists or creates it if not to discord webhook
+                if exists(f"config/embeds/{streamer}.txt"):
+                    logger.info('embed allready exsists for %s with name %s, updating it',streamer, streamer_name)
+                    message_id_from_file,name_from_file = read_message_id_from_file(streamer)
+                    discord_webhook_edit(get_stream_json_from_twitch_data,message_id_from_file)
                 else:
-                    # removes embed if offline or uses offline message
-                    if exists(f"config/embeds/{streamer}.txt"):
-                        logger.info('%s with name %s is no longer live',streamer, streamer_name)
-                        message_id_from_file,name_from_file = read_message_id_from_file(streamer)
-                        
-                        if loaded_config.use_offline_messages:
-                            discord_webhook_edit_to_offline(message_id_from_file,name_from_file)
-                        else:
-                            discord_webhook_delete(message_id_from_file)
-                        remove_message_id_file(streamer)
-        
-        logger.info('finished main loop, waiting for %s minutes',loaded_config.poll_interval)
-        time.sleep(poll_interval_minutes)
+                    logger.info('no embed exsists for %s with name %s, creating it',streamer, streamer_name)
+                    message_id = discord_webhook_send(get_stream_json_from_twitch_data)
+                    save_message_id_to_file(streamer,message_id,streamer_name)
+            
+            else:
+                # removes embed if offline or uses offline message
+                if exists(f"config/embeds/{streamer}.txt"):
+                    logger.info('%s with name %s is no longer live',streamer, streamer_name)
+                    message_id_from_file,name_from_file = read_message_id_from_file(streamer)
+                    
+                    if loaded_config.use_offline_messages:
+                        discord_webhook_edit_to_offline(message_id_from_file,name_from_file)
+                    else:
+                        discord_webhook_delete(message_id_from_file)
+                    remove_message_id_file(streamer)
+    
+    logger.info('finished main loop, waiting for %s minutes',loaded_config.poll_interval)
+    time.sleep(poll_interval_minutes)
 
 if __name__ == "__main__":
     main()
