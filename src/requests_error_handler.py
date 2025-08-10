53d69ceac7b9afb3ac6312ec5e7188c74fcd49e9
keyboardmedic <keyboardmedic@proton.me>
2025-08-10 19:23:33 +0200

    fix: added needed parameter to handle_request_exception

--- src/requests_error_handler.py
+++ src/requests_error_handler.py
@@ -9,11 +9,13 @@ def init_error_handler() -> tuple[int, int, int]:
 def handle_response_not_ok(error_count: int) -> tuple[int, int]:
     error_count = error_count+1
     remaining_errors = max_errors_allowed-error_count
+
     return(error_count, remaining_errors)
     
-def handle_request_exception():
+def handle_request_exception(error_count: int) -> tuple[int, int]:
     error_count = error_count+1
     remaining_errors = max_errors_allowed-error_count
+
     return(error_count, remaining_errors)
     
 def raise_no_more_tries_exception(max_errors_allowed: int):
