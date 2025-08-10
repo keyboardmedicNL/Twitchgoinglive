446b21fbc5e71cf4bace049ed5d0723db11d05a0
keyboardmedic <keyboardmedic@proton.me>
2025-08-10 18:17:54 +0200

    refactor: adjusted spacing for readability

--- src/twitch_api_handler.py
+++ src/twitch_api_handler.py
@@ -12,14 +12,18 @@ max_errors_allowed = 3
 
 # renews token used for twitch api calls
 def get_token_from_twitch_api() -> str:
+        
         error_count = 0
+
         while error_count < max_errors_allowed:
+
             try:
                 logging.info("Requesting new api auth token from twitch")
 
                 get_token_from_twitch_request=requests.post("https://id.twitch.tv/oauth2/token", json={"client_id" : str(loaded_config.twitch_api_id), "client_secret" : str(loaded_config.twitch_api_secret), "grant_type":"client_credentials"})
                 
                 if get_token_from_twitch_request.ok:
+
                     get_token_from_twitch_json = get_token_from_twitch_request.json()
                     token_from_twitch = get_token_from_twitch_json["access_token"]
                     logging.debug("new twitch api auth token recieved")
@@ -33,6 +37,7 @@ def get_token_from_twitch_api() -> str:
                     if not error_count == max_errors_allowed:
                         logging.error("unable to request new twitch api auth token with response: %s trying %s more times and waiting for %s seconds",get_token_from_twitch_request, remaining_errors , time_before_retry)
                         time.sleep(time_before_retry)
+
             except Exception as e:
                 error_count = error_count+1
                 remaining_errors = max_errors_allowed-error_count
@@ -40,14 +45,18 @@ def get_token_from_twitch_api() -> str:
                 if not error_count == max_errors_allowed:
                     logging.error("unable to request new twitch api auth token with exception: %s trying %s more times and waiting for %s seconds", e, remaining_errors, time_before_retry)
                     time.sleep(time_before_retry)
+
         if error_count == max_errors_allowed:
             raise RuntimeError("Unable to request a new twitch api token after trying 3 times.")
         
 
 # gets stream information from twitch api
 def get_stream_json_from_twitch(streamer: str, token_from_twitch: str) -> tuple[dict, dict, bool, str, str]:
+
     error_count = 0
+
     while error_count < max_errors_allowed:
+        
         try:
             get_stream_json_from_twitch_request=requests.get(f"https://api.twitch.tv/helix/streams?&user_id={streamer}", headers={'Authorization':f"Bearer {token_from_twitch}", 'Client-Id':loaded_config.twitch_api_id})
             logging.debug("tried to get streamer information for %s with response: %s",streamer, get_stream_json_from_twitch_request)
@@ -62,11 +71,13 @@ def get_stream_json_from_twitch(streamer: str, token_from_twitch: str) -> tuple[
                         streamer_name = get_stream_json_from_twitch_request_json["data"][0]["user_name"]
                         logging.info("%s with name %s is live and in category %s",streamer, streamer_name, stream_category)
                     break
+
                 except:
                     is_live = False
                     stream_category = ""
                     streamer_name = ""
                     break
+
             else:
                 error_count = error_count+1
                 remaining_errors = max_errors_allowed-error_count
@@ -82,6 +93,7 @@ def get_stream_json_from_twitch(streamer: str, token_from_twitch: str) -> tuple[
             if not error_count == max_errors_allowed:
                 logging.error("tried to get streamer information for %s with exception: %s trying %s more times and waiting for %s seconds",streamer, e, remaining_errors , time_before_retry)
                 time.sleep(time_before_retry)
+
     if error_count == max_errors_allowed:
         raise RuntimeError("Unable to get streamer info from twitch after trying 3 times.")
 
@@ -89,8 +101,10 @@ def get_stream_json_from_twitch(streamer: str, token_from_twitch: str) -> tuple[
 
 # get list of team member uids
 def get_list_of_team_member_uids(team_name: str, api_token: str) -> list:
+
     error_count = 0
     while error_count < max_errors_allowed:
+
         try:
             logging.info("getting team data from twitch api for team %s", team_name)
 
