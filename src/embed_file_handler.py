a4d54b25dd7ae6256e612cf781e9844d090c7ab4
keyboardmedic <keyboardmedic@proton.me>
2025-08-10 18:16:17 +0200

    refactor: removed unnneeded comments

--- src/embed_file_handler.py
+++ src/embed_file_handler.py
@@ -5,14 +5,12 @@ import logging
 # vars
 loaded_config = config_loader.load_config()
 
-# saves streamid to file
 def save_message_id_to_file(name: str ,message_id: str ,user_login: str, embed_color: str, user_name: str):
     with open(f"config/embeds/{name}.txt", 'w') as file_to_save_message_id_to:
         file_to_save_message_id_to.write(message_id + '\n' + user_login + '\n' + embed_color + '\n' + user_name)
 
     logging.debug("message id: %s and name: %s and color: %s and username: %s saved in file %s.txt",message_id, user_login, embed_color, user_name, name)
 
-# reads streamid from file
 def read_message_id_from_file(name: str) -> tuple[str, str, str, str]:
     with open(f"config/embeds/{name}.txt", 'r') as file_to_read_from:
         discord_webhook_message_id = str(file_to_read_from.readline())
@@ -27,7 +25,6 @@ def read_message_id_from_file(name: str) -> tuple[str, str, str, str]:
 
     return(discord_webhook_message_id, name_in_file, embed_color, user_name)
 
-# remove file
 def remove_message_id_file(name: str):
     os.remove(f"config/embeds/{name}.txt")
 
